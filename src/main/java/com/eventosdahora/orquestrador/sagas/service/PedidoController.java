3857e03620ed34ad75e01f16dc3563136e5b773d
Matheus <matheus.etb@gmail.com>
2020-10-07 00:15:33 -0300

    Ajuste no método post

--- src/main/java/com/eventosdahora/orquestrador/sagas/service/PedidoController.java
+++ src/main/java/com/eventosdahora/orquestrador/sagas/service/PedidoController.java
@@ -2,11 +2,12 @@ package com.eventosdahora.orquestrador.sagas.service;
 
 import com.eventosdahora.orquestrador.sagas.dto.OrderDTO;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/pedido")
 public interface PedidoController {
 	
 	@PostMapping
-	void novoPedido(OrderDTO orderDTO);
+	void novoPedido(@RequestBody OrderDTO orderDTO);
 }
